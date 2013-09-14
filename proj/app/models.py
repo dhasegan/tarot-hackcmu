9f96d71489ac21b5e8a65ee87ae43f1c249163f0
Daniel Hasegan <danielhasegan@gmail.com>
2013-09-14 14:44:21 -0400

    Database model added

--- proj/app/models.py
+++ proj/app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class QUser(User):
+	score = models.FloatField()
+	weight = models.FloatField()
+
+class Question(models.Model):
+	text = models.CharField(max_length=140)
+	description = models.CharField(max_length=1000)
+	timeStart = models.DateField(auto_now=True)
+	timeEnd = models.DateField()
+	minValue = models.FloatField()
+	maxValue = models.FloatField()
+	upvote = models.IntegerField()
+	downvote = models.IntegerField()
+
+class Answer(models.Model):
+	user = models.ForeignKey(BidUser);
+	question = models.ForeignKey(Question)
+	value = models.FloatField()
+	timeSubmit = models.DateField(auto_now=True)
--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'proj.views.home', name='home'),
-    # url(r'^proj/', include('proj.foo.urls')),
+    url(r'^/', include('app.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
