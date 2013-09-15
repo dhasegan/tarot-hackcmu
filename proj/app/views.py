5a465b27e82eb2e0707b69a84a07b3c1c3b50cf3
NitinSing <nitinsingh.nitt@gmail.com>
2013-09-14 20:32:24 -0400

    Made the login page

--- proj/app/views.py
+++ proj/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 
+from django.http import HttpResponseRedirect
+
 # Decorator to use built-in authentication system
 from django.contrib.auth.decorators import login_required
 
@@ -27,6 +29,38 @@ def add_question(request):
 	context = {}
 	return render(request, 'types/question.html', context)
 
-def login(request):
-	context = {}
-	return render(request, 'pages/login.html', context)
+def login(request):        
+    context = { 'page': "login" }       
+
+    errors = []
+    context['errors'] = errors
+        
+    # username
+    if not 'userName' in request.POST:  
+        # print "no username in request"
+        errors.append('Username is required.')        
+    if not 'userPassword' in request.POST:          
+        # print "no password in request"
+        errors.append('Password is required.')        
+    else:       
+        if (len(QUser.objects.filter(username = request.POST['userName'])) > 0):            
+            user = QUser.objects.filter(username = request.POST['userName'])[0]
+            if (user.check_password(request.POST['userPassword'])):
+            #print "username found in db"
+                return HttpResponseRedirect("pages/discover.html")            
+            # errors.append('Username is already taken.')                    	
+        else:
+            if (len(request.POST['userPassword']) > 0):
+                new_user = QUser.objects.create_user(username=request.POST['userName'], \
+                    password=request.POST['userPassword'],score=0,weight=0)        	            
+                # print "new username is created"
+                new_user.save()
+                # print "username saved"
+                # return redirect('pages/discover.html')
+                return HttpResponseRedirect("pages/discover.html")               
+
+    if errors:
+        # print "some errors"
+        return render(request, 'pages/login.html', context)        
+    
+    return render(request, 'pages/login.html', context)
