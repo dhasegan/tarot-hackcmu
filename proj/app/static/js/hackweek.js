c7b0dd2c34cfad9f8bea16dfb21aaf3cf6962612
Chris Ying <chrsyng@gmail.com>
2013-09-14 20:25:44 -0400

    I added a short bit for timers, you'll need to replace my comments with actual JS code

--- proj/app/static/js/hackweek.js
+++ proj/app/static/js/hackweek.js
@@ -1,4 +1,20 @@
+var timer = setInterval(function() {
+	/* Code that goes here:
+	 * Get the counter numbers from the html
+	 * Decrease that number by 1
+	 * Set the counter on the html to this new number
+	 * *Note* for now, keep the time in terms of seconds ONLY
+	 */
+}, 1000);
 
+setTimeout(function() {
+	clearInterval(timer); //Stops the ticking of the timer
+	/* Code that goes here:
+	 * Removes the question or disables voting somehow
+	 * Runs the python script that takes the votes and user scores
+	 * and updates the database with the new user private/public scores
+	 */
+}, 60000); //Replace 60000 with how long the question lasts in milliseconds
 
 $('.ui-slider-handle').draggable({ axis: 'x', containment: "parent", stop: function() {
 	var qcur = $(this).parents('.qvalue');
